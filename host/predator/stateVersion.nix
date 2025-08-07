7b44a166d9f427971c20fb04167fa876f6fb2741
Markus John <johm@quyo.de>
2025-08-07 16:02:48 +0000

    Clean up wsl configs

--- host/predator/stateVersion.nix
+++ host/predator/stateVersion.nix
@@ -1,18 +1,6 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
-# NixOS-WSL specific options are documented on the NixOS-WSL repository:
-# https://github.com/nix-community/NixOS-WSL
-
-{ config, lib, pkgs, hostname, ... }:
+{ config, lib, pkgs, ... }:
 
 {
-  wsl.enable = true;
-  wsl.defaultUser = "johm";
-
-  networking.hostName = hostname;
-
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It's perfectly fine and recommended to leave
--- role/wsl/nixos-wsl.nix
+++ role/wsl/nixos-wsl.nix
@@ -1,7 +1,14 @@
-{ config, lib, pkgs, inputs, ... }:
+{ config, lib, pkgs, inputs, hostname, ... }:
 
 {
+
   imports = [
     inputs.nixos-wsl.nixosModules.default
   ];
+
+  wsl.enable = true;
+  wsl.defaultUser = "johm";
+
+  networking.hostName = hostname;
+
 }
