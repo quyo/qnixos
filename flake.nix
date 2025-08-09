0356cb063570be33457a3e0c46bd4211265f1a89
Markus John <johm@quyo.de>
2025-08-09 11:18:04 +0000

    Update and simplify flake inputs

--- flake.nix
+++ flake.nix
@@ -17,7 +17,9 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     nixpkgs-unstable.url = "github:NixOS/nixpkgs/nixos-unstable";
+
     nixos-wsl.url = "github:nix-community/NixOS-WSL/release-25.05";
+    nixos-wsl.inputs.nixpkgs.follows = "nixpkgs";
 
     qnixpkgs.url = "github:quyo/qnixpkgs";
     qnixpkgs.inputs.nixpkgs-stable.follows = "nixpkgs";
