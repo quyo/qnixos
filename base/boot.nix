fc48886ad9c33723c98527f360c1738d36656f62
Markus John <johm@quyo.de>
2025-08-16 11:09:19 +0000

    Fix conflicting definition values

--- base/boot.nix
+++ base/boot.nix
@@ -2,7 +2,7 @@
 
 {
 
-  boot.loader.grub.enable = true;
+  boot.loader.grub.enable = lib.mkOptionDefault true;
   boot.loader.grub.device = "/dev/sda";
   boot.loader.grub.useOSProber = true;
   boot.loader.grub.configurationLimit = 10;
