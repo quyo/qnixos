e3764cf8936e5d8f5d5e8bb31a47c34962994df3
Markus John <johm@quyo.de>
2025-08-16 12:04:24 +0000

    Add johm@nyx to authorized keys

--- base/user/johm.nix
+++ base/user/johm.nix
@@ -8,8 +8,9 @@
     extraGroups = [ "networkmanager" "wheel" "docker" ];
     packages = with pkgs; [];
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMkccByEYMQ2vXmSkU9x0WJpnLDseu8wFSKiPQCjH/Kt johm@Predator"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDJbNZ7WPukcyJs8KSPn8/LGfk4knwicscIBIhKAuEh6 johnm@DE-ADN-2TYD893"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMkccByEYMQ2vXmSkU9x0WJpnLDseu8wFSKiPQCjH/Kt johm@predator"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPbe3UzNIWAtDSZD0IBGBldV6BbHo0LGPVLDC/PXPaMR johm@nyx"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDJbNZ7WPukcyJs8KSPn8/LGfk4knwicscIBIhKAuEh6 johnm@de-adn-2tyd893"
     ];
   };
 
