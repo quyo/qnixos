ab126ad6e27655298567f921f4d937bb0f551dcd
Markus John <johm@quyo.de>
2025-08-16 19:42:38 +0000

    Update qnixos-secrets

--- base/net/postfix.nix
+++ base/net/postfix.nix
@@ -2,8 +2,8 @@
 
 {
 
-  age.secrets.postfix_smtp_sasl_password_maps = {
-    file = "${secrets}/postfix_smtp_sasl_password_maps.age";
+  age.secrets.postfix-smtp_sasl_password_maps = {
+    file = "${secrets}/postfix-smtp_sasl_password_maps.age";
     path = "/var/lib/postfix/secret/smtp_sasl_password_maps";
     symlink = false;
     owner = "root";
@@ -20,7 +20,7 @@
       relayhost = [mx.quyo.net]:587
       smtp_tls_security_level = encrypt
       smtp_sasl_auth_enable = yes
-      smtp_sasl_password_maps = texthash:${config.age.secrets.postfix_smtp_sasl_password_maps.path}
+      smtp_sasl_password_maps = texthash:${config.age.secrets.postfix-smtp_sasl_password_maps.path}
       smtp_sasl_security_options = noanonymous
     '';
   };
