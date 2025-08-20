d50ee74f655f617ca014337b5853e8992c9c88c4
Markus John <johm@quyo.de>
2025-08-20 10:17:19 +0000

    Update qnixos-secrets; fix permissions for nix-access-tokens

--- base/nix/nix-access-tokens.nix
+++ base/nix/nix-access-tokens.nix
@@ -4,8 +4,8 @@
 
   age.secrets.base-nix-access-tokens = {
     file = "${secrets}/secrets/base/nix/nix-access-tokens.age";
-    owner = "root";
-    group = "root";
+    owner = "johm";
+    group = "users";
     mode  = "0400";
   };
 
