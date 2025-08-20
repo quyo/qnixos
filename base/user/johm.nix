98f51c1a85504622884729a7d06e22533cc360a1
Markus John <johm@quyo.de>
2025-08-20 08:04:54 +0000

    Build authorizedKeys from qnixos-secrets

--- base/user/johm.nix
+++ base/user/johm.nix
@@ -1,4 +1,4 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, secrets, ... }:
 
 {
 
@@ -7,11 +7,20 @@
     description = "Markus John";
     extraGroups = [ "networkmanager" "wheel" "docker" ];
     packages = with pkgs; [];
-    openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMkccByEYMQ2vXmSkU9x0WJpnLDseu8wFSKiPQCjH/Kt johm@predator"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPbe3UzNIWAtDSZD0IBGBldV6BbHo0LGPVLDC/PXPaMR johm@nyx"
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDJbNZ7WPukcyJs8KSPn8/LGfk4knwicscIBIhKAuEh6 johnm@de-adn-2tyd893"
-    ];
+    openssh.authorizedKeys.keys =
+      let
+        userkey = host: builtins.readFile "${secrets}/secrets/hosts/${host}/ssh/ssh_johm_ed25519_key.pub";
+      in
+      [
+        # OBSOLETE
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIDJbNZ7WPukcyJs8KSPn8/LGfk4knwicscIBIhKAuEh6 johnm@de-adn-2tyd893"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPbe3UzNIWAtDSZD0IBGBldV6BbHo0LGPVLDC/PXPaMR johm@nyx"
+        "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIMkccByEYMQ2vXmSkU9x0WJpnLDseu8wFSKiPQCjH/Kt johm@predator"
+      ] ++ map userkey [
+        "de-adn-2tyd893"
+        "nyx"
+        "predator"
+      ];
   };
 
 }
