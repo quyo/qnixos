71e4987ee8205a33febd6a456ba34bf7d9a7900d
Markus John <johm@quyo.de>
2025-08-22 06:49:22 +0000

    Refactor packages/proxmox-lxc-template.nix

--- packages/proxmox-lxc-template.nix
+++ packages/proxmox-lxc-template.nix
@@ -1,17 +1,21 @@
 { inputs, system }:
 
 let
-  nixos-generators = inputs.nixos-generators;
-  extendSpecialArgs = inputs.self.outputs.extendSpecialArgs;
-  nixosModules = inputs.self.outputs.nixosModules;
+  baseTarball =
+    let
+      nixos-generators = inputs.nixos-generators;
+      extendSpecialArgs = inputs.self.outputs.extendSpecialArgs;
+      nixosModules = inputs.self.outputs.nixosModules;
+    in
+      nixos-generators.nixosGenerate {
+        inherit system;
+        format = "proxmox-lxc";
+
+        specialArgs = extendSpecialArgs { inherit system; hostname = "proxmox-lxc"; };
+
+        modules = [
+          nixosModules.proxmox-lxc
+        ];
+      };
 in
-  nixos-generators.nixosGenerate {
-    inherit system;
-    format = "proxmox-lxc";
-
-    specialArgs = extendSpecialArgs { inherit system; hostname = "proxmox-lxc"; };
-
-    modules = [
-      nixosModules.proxmox-lxc
-    ];
-  }
+  baseTarball
